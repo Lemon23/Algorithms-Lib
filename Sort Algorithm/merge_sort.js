7892aec8283a4663b7111b1caeb48478a1b7905f
Lemon23 <L_Qiao23@163.com>
2016-12-28 17:14:49 +0800

    归并排序

--- Sort Algorithm/merge_sort.js
+++ Sort Algorithm/merge_sort.js
@@ -17,14 +17,14 @@ function merge(left, right) {
 	var result = [];
 	while(left.length > 0 && right.length > 0) {
 		if(left[0] < right[0]) {
-			console.log('1----' + result + ' left: ' + left + ' right: ' + right)
+			// console.log('1----' + result + ' left: ' + left + ' right: ' + right)
 			result.push(left.shift())
 		} else {
-			console.log('2----' + result + ' left: ' + left + ' right: ' + right)
+			// console.log('2----' + result + ' left: ' + left + ' right: ' + right)
 			result.push(right.shift())
 		}
 	}
-	console.log('3-----' + result)
+	// console.log('3-----' + result)
 	return result.concat(left).concat(right);
 }
 
